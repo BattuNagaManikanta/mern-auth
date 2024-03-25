bc7dd3da03cdf9bc6ed9433abbfa16ae200c3367
Manikana <maniking22000@gmail.com>
2024-03-25 18:13:18 +0530

    added firebase storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { app } from '../Firebase';
+import {getStorage,ref, uploadBytesResumable,getDownloadURL} from 'firebase/storage'
 
 export default function Profile() {
   const fileRef=useRef(null);
+  const [image,setImage]=useState(undefined);
+  const [imagePercent,setImagePercent]=useState(0)
+  const [imageError,setImageError]=useState(false)
+  const [formData,setFormData]=useState({});
   const {currentUser}=useSelector((state)=>state.user);
+  console.log(imagePercent);
+  console.log(image);
+  console.log(formData)
+  console.log(imageError)
+  useEffect(()=>{
+    if(image){
+      handleFileUpload(image)
+    }
+  },[image])
+  const handleFileUpload= async ()=>{
+    const storage=getStorage(app);
+    const fileName= new Date().getTime() + image.name;
+    const storageRef=ref(storage,fileName);
+    const uploadTask=uploadBytesResumable(storageRef,image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot)=>{
+        const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+        setImagePercent(Math.round(progress))
+      },
+      (error)=>{
+        console.log(error)
+        setImageError(true)
+
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        ); 
+      })
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <input className='hidden' type='file' ref={fileRef} accept="image/*"/>
+        <input className='hidden' type='file' ref={fileRef} accept="image/*" onChange={(e)=>{ setImage(e.target.files[0])}}/>
         <img className='h-24 w-24 self-center rounded-full object-cover cursor-pointer' src={currentUser.profilePicture} onClick={()=>fileRef.current.click()}></img>
+        <p className='text-sm self-center'>
+          {imageError ? (
+            <span className='text-red-700'>
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className='text-slate-700'>{`Uploading: ${imagePercent} %`}</span>
+          ) : imagePercent === 100 ? (
+            <span className='text-green-700'>Image uploaded successfully</span>
+          ) : (
+            ''
+          )}
+        </p>
         <input defaultValue={currentUser.username}type="text" id="username" placeholder='Username' className='bg-slate-100 rounded-lg p-3'></input>
         <input defaultValue={currentUser.email} type="text" id="email" placeholder='Email' className='bg-slate-100 rounded-lg p-3'></input>
         <input type="password" id="password" placeholder='Password' className='bg-slate-100 rounded-lg p-3'></input>
